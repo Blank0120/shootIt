d3571e3043d4f3212eb19c303ae2f37efb03a9c9
邓英杰 <dengyj@chinagdn.com>
2022-11-17 11:33:59 +0800

    feat: create enemies

--- src/core/core.ts
+++ src/core/core.ts
@@ -48,4 +48,33 @@ class Projectile {
 	}
 }
 
-export { Player, Projectile }
+class Enemy {
+	x: number;
+	y: number;
+	radius: number;
+	color: string;
+	velocity: { x: number, y: number };
+
+	constructor(x: number, y: number, radius: number, color: string, velocity: {x: number, y: number}) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.velocity = velocity;
+	};
+
+	draw(ctx: CanvasRenderingContext2D) {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+
+	update(ctx: CanvasRenderingContext2D) {
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+		this.draw(ctx);
+	}
+}
+
+export { Player, Projectile, Enemy }
